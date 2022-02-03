a767fd813bdb0532fff7949754eb330e6ecb707f
Pavel <araa9977@mail.ru>
2022-02-03 07:30:48 +0700

    Главный класс коммандны приложения

--- CV19/Infrastructure/Commands/Base/LambdaCommand.cs
+++ CV19/Infrastructure/Commands/Base/LambdaCommand.cs
@@ -6,20 +6,17 @@ namespace CV19.Infrastructure.Commands.Base
 {
     internal class LambdaCommand : Command
     {
-        internal class LambdaCommand : Command
+        private readonly Action<object> _Execute;
+        private readonly Func<object, bool> _CanExecute;
+
+        public LambdaCommand(Action<object> Execute, Func<object, bool> CanExecute = null)
         {
-            private readonly Action<object> _Execute;
-            private readonly Func<object, bool> _CanExecute;
-
-            public LambdaCommand(Action<object> Execute, Func<object, bool> CanExecute = null)
-            {
-                _Execute = Execute ?? throw new ArgumentNullException(nameof(Execute));
-                _CanExecute = CanExecute;
-            }
-
-            public override bool CanExecute(object parameter) => _CanExecute?.Invoke(parameter) ?? true;
-
-            public override void Execute(object parameter) => _Execute(parameter);
+            _Execute = Execute ?? throw new ArgumentNullException(nameof(Execute));
+            _CanExecute = CanExecute;
         }
+
+        public override bool CanExecute(object parameter) => _CanExecute?.Invoke(parameter) ?? true;
+
+        public override void Execute(object parameter) => _Execute(parameter);
     }
 }
