571be7abc93800622bce053a90b5c08a223bf3d8
Pavel <araa9977@mail.ru>
2022-05-25 07:55:49 +0700

    Конвертеры, ресурсы кистей, блокировка Freezable-объектов в разметке для повышения быстродействия

---
+++ CV19/Infrastructure/Converters/Ratio.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace CV19.Infrastructure.Converters
+{
+    internal class Ratio : Converter
+    {
+        public Ratio() { }
+
+        public Ratio(double k) => K = k;
+
+        public double K { get; set; } = 1;
+        public override object Convert(object value, Type t, object p, CultureInfo c)
+        {
+            if (value is null) return null;
+
+            var x = System.Convert.ToDouble(value, c);
+
+            return x * K;
+        }
+
+        public override object ConvertBack(object value, Type t, object p, CultureInfo c)
+        {
+            if (value is null) return null;
+
+            var x = System.Convert.ToDouble(value, c);
+
+            return x / K;
+        }
+    }
+}
