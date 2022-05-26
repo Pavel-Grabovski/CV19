bfdbd2b6abbc1e46226d65e70a3f437fa2711138
Pavel <araa9977@mail.ru>
2022-05-27 05:38:56 +0700

    Мульти-конвертер

---
+++ CV19/Infrastructure/Converters/ToArray.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace CV19.Infrastructure.Converters
+{
+    internal class ToArray : MultiConverter
+    {
+        public override object Convert(object[] v, Type t, object p, CultureInfo c)
+        {
+            var collection = new CompositeCollection();
+            foreach(var value in v)
+            {
+                collection.Add(value);
+            }
+            return collection;
+        }
+    }
+}
