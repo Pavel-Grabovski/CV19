0339114c8e936fca21d35806a9299d4f1df37a4c
Pavel <araa9977@mail.ru>
2022-06-22 15:31:20 +0700

    Собственная точка входа в приложение - метод Program.Main

---
+++ CV19/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CV19
+{
+    public static class Program
+    {
+        [STAThread]
+        public static void Main()
+        {
+            var app = new App();
+            app.InitializeComponent();
+            app.Run();
+        }
+    }
+}
