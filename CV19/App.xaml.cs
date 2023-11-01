7aac65d64225a3d204aff181067de27c1e57a11f
Павел Грабовский <araa9977@mail.ru>
2023-11-01 10:51:21 +0700

    Инфраструктура для создания хоста приложения

--- CV19/App.xaml.cs
+++ CV19/App.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System.Windows;
+﻿using CV19.Services;
+using CV19.ViewModels;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System.Windows;
 
 namespace CV19
 {
@@ -7,16 +11,18 @@ namespace CV19
     /// </summary>
     public partial class App : Application
     {
-        public static bool IsDesignMode
-        {
-            get;
-            private set;
-        } = true;
+        public static bool IsDesignMode{ get; private set; } = true;
 
         protected override void OnStartup(StartupEventArgs e)
         {
             IsDesignMode = false;
             base.OnStartup(e);
+        }
+
+        public static void ConfigureServices(HostBuilderContext host, IServiceCollection services)
+        {
+            services.AddSingleton<DataService>();
+            services.AddSingleton<CountriesStatisticViewModel>();
         }
     }
 }
